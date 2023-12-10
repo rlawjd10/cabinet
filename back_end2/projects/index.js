bfdba921776d15fc49925e56eaf27a875f75460c
EunJaeJo <joeunjae0925@gmail.com>
2023-12-10 17:01:53 +0900

    [REFACTOR] Update server URL and port number

--- back_end2/projects/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('./config/express');
-const { logger } = require('./config/winston');
-
-const port = 3000;
-const serverURL = 'http://34.64.116.143';
-
-express().listen(port);
-
-logger.info(`${process.env.NODE_ENV} - API Server Start At ${serverURL}:${port}`);
--- back_end2/projects/src/app/assignment/assignRoute.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app){
-    const assignment = require('./assignController');
-
-    // 1. 출결 조회 API
-    app.get('/app/:groupId/assignment', assignment.getAssginment);
-
-};
