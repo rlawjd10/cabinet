50d325619bec500426fe3097409f5de3f7894ad6
E_Jaeae <80445063+EunjaeJo@users.noreply.github.com>
2023-12-12 19:14:53 +0900

    [CHORE]Add commend to index.js

--- back_end/projects/index.js
+++ back_end/projects/index.js
@@ -2,7 +2,7 @@ const express = require('./config/express');
 const { logger } = require('./config/winston');
 
 const port = 3000;
-const serverURL = 'http://34.64.116.143';
+const serverURL = 'http://34.64.116.143'; // 서버URL
 
 express().listen(port);
 
